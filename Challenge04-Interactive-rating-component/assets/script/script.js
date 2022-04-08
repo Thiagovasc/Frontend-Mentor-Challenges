8318d25e78187fbf167641b94f54db85399db432
Thiago Vasconcelos <pedrosav14@gmail.com>
2022-04-08 19:01:52 -0300

    Span still doesn't work

--- Challenge04-Interactive-rating-component/assets/script/script.js
+++ Challenge04-Interactive-rating-component/assets/script/script.js
@@ -1,9 +1,28 @@
-function ratingButton(id){
-    var element = document.getElementById(id)
-    var spanNote = document.getElementsByClassName("selected-rating")
+let ratinglist = document.getElementsByClassName("rating-list")[0].children 
+let maindiv = document.getElementsByClassName('container-support')[0]
+let notediv = document.getElementsByClassName('card-state-active')[0]
 
-    var note = id
-    spanNote.innerHTML = id
-    element.style.background = "orange"
+
+var userNote
+let noteSelected = false
+const note = document.getElementsByClassName('selected-rating');
+
+function ratingButton(id) {
+
+    userNote = id
+
+    for (let i = 0; i < ratinglist.length; i++) {
+       id === i ? (addingClass(ratinglist[i]), noteSelected = true) : addingClass(ratinglist[i])
+    }
+
+}
+
+function addingClass(element) {
+    element.classList.contains('selected-yet') ? element.classList.remove('selected.yet') :     element.classList.add('selected.yet')
+}
+
+
+function submitButton() {
+    noteSelected == true ? (maindiv.style.display = 'none', notediv.style.display = 'block', note.innerHTML = userNote + 1) : alert("Selecione uma nota")
 
 }
